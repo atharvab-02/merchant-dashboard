638ffef8550099a2c431553a6b13aabffe171876
Atharva Bhanage <129030955+atharvab-02@users.noreply.github.com>
2023-12-22 09:15:48 +0530

    add : added `constants` file

---
+++ lib/constants/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const Color primary = Color(0xFF3267CF);
+const Color bodyBg = Color(0xFFF7F7F7);
+const Color green = Color(0xFF28A650);
+const Color grey = Color(0xFF737373);
+const Color white = Color(0xFFFFFFFF);
+const Color lightgrey = Color(0xFF888888);
+const Color lightBlue = Color(0xFF4779DB);
+const Color darkgrey = Color(0xFF8FB4FC);
+const Color greyf2f2 = Color(0xFFF2F2F2);
+const Color greyf9f9 = Color(0xFFF9F9F9);
+const Color grey7373 = Color(0xFF737373);
+const Color greydede = Color(0xFFDEDEDE);
